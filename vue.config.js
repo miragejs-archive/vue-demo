5ce22788641c706578bb0bcbf787cc054db286e9
Ryan Toronto <ryanto@gmail.com>
2020-01-27 13:57:21 -0500

    add null loader

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  chainWebpack: (config) => {
-    config.resolve.symlinks(false)
+  chainWebpack: config => {
+    config.resolve.symlinks(false);
+
+    // if (process.env.NODE_ENV === "production") {
+    //   config.module
+    //     .rule("exclude-mirage")
+    //     .test(/node_modules\/miragejs/)
+    //     .use("null-loader")
+    //     .loader("null-loader");
+    // }
   }
-}
+};
