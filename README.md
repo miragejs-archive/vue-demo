8a74974350005518c3e6324c6479498f42b4e564
Ryan T <ryanto@gmail.com>
2020-01-27 14:22:33 -0500

    Remove mirage from production builds (#1)
    Keep it included in the netlify build with env var

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Mirage Vue demo
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/1d4aa665-ac38-4fb1-996e-6de242c0800a/deploy-status)](https://app.netlify.com/sites/mirage-vue-demo/deploys)
+
 To see the demo, run
 
 ```sh
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
 module.exports = {
-  chainWebpack: (config) => {
-    config.resolve.symlinks(false)
+  pluginOptions: {
+    webpackBundleAnalyzer: {
+      openAnalyzer: false
+    }
+  },
+
+  chainWebpack: config => {
+    config.resolve.symlinks(false);
+
+    if (
+      process.env.NODE_ENV === "production" &&
+      process.env.MIRAGE_ENABLED !== "true"
+    ) {
+      config.module
+        .rule("exclude-mirage")
+        .test(/node_modules\/miragejs\//)
+        .use("null-loader")
+        .loader("null-loader");
+    }
   }
-}
+};
