7a87e8b66c282e23743df12818dc4a2a16451cda
Jan Eivind Rognås <jan.rognas@byr.oslo.kommune.no>
2020-08-10 10:35:57 +0200

    Setup jest and mocha

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,31 @@
+import { Server, Model } from "miragejs"
+import axios from "axios"
+
+describe.only('testing', () => {
+    it.only("routes config option works", async () => {
+        
+        let server = new Server({
+            environment: "test",
+            logging: true,
+            models: {
+                user: Model
+            },
+            routes() {
+                this.namespace = "api";
+
+                this.get("/users");
+            }
+        });
+        server.createList("user", 3);
+        let response = await axios.get("/api/users");
+        // let data = await axios.get("/api/users");
+        expect(response.data).toEqual({
+            users: [{ id: "1" }, { id: "2" }, { id: "3" }]
+        });
+        server.shutdown()
+    });
+
+  
+
+
+})
